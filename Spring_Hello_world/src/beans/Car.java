36c36c4b36adef08e03c6dc314029e8c851dd701
dashy <dashy@DESKTOP-HSQ84PL>
2020-02-09 19:39:54 +0530

    Dependency Check implementation

--- Spring_Hello_world/src/beans/Car.java
+++ Spring_Hello_world/src/beans/Car.java
@@ -1,30 +1,28 @@
 package beans;
 
 public class Car {
-	private String carName[];
-	private Engine engine[];
-	public String[] getCarName() {
+	private String carName;
+	private Engine engine;
+	public String getCarName() {
 		return carName;
 	}
-	public void setCarName(String carName[]) {
+	public void setCarName(String carName) {
 		this.carName = carName;
 	}
-	public Engine[] getEngine() {
+	public Engine getEngine() {
 		return engine;
 	}
-	public void setEngine(Engine engine[]) {
+	public void setEngine(Engine engine) {
 		this.engine = engine;
 	}	
 	public Car(){
 		
 	}
-	public Car(String carName[],Engine engine[]) {
+	public Car(String carName,Engine engine) {
 		this.carName=carName;
 		this.engine = engine;
 	}
 	public void print() {
-		for(int i=0;i<carName.length;i++) {
-			System.out.println(carName[i]+" :: "+engine[i].getModelYear());
-		}		
+		System.out.println(carName+" :: "+engine.getModelYear());
 	}
 }
