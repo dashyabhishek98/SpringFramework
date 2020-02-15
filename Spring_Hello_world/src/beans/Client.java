6e719b8853371be59b1d601b7335fb8366fa9b99
dashy <dashy@DESKTOP-HSQ84PL>
2020-02-15 16:49:26 +0530

    Static Variable Dependency Injection

--- Spring_Hello_world/src/beans/Client.java
+++ Spring_Hello_world/src/beans/Client.java
@@ -7,28 +7,8 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String files[] = {"resources/Engine.xml","resources/Car.xml"};
-		ApplicationContext factory = new ClassPathXmlApplicationContext(files);
-		Car c = (Car)factory.getBean("c");
-		c.print();
-		/*Resource r = new ClassPathResource("resources/Spring.xml");
-		BeanFactory factory = new XmlBeanFactory(r);
-		Object o =factory.getBean("t");
-		Test test = (Test)o;
-		test.hello("Abhishek");*/
-		/*try {
-		Class c = Class.forName("beans.Test");
-		Constructor cons[] = c.getDeclaredConstructors();
-		cons[0].setAccessible(true);
-		Object o = cons[0].newInstance(null);
-		Test t = (Test)o;
-		t.hello();
-		}
-		catch(Exception e) {
-			
-		}*/
-		/*System.out.println(o==o1);
-		System.out.println(o==o2);*/
+		ApplicationContext factory = new ClassPathXmlApplicationContext("resources/Spring.xml");
+		Test t1 =(Test)factory.getBean("t");		
 	}
 
 }
