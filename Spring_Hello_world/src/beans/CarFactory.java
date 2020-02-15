832ad2dbfb7c59f201e116396684c20aa443c95f
dashy <dashy@DESKTOP-HSQ84PL>
2020-02-15 20:46:04 +0530

    Instance Factory Approach

--- Spring_Hello_world/src/beans/CarFactory.java
+++ Spring_Hello_world/src/beans/CarFactory.java
@@ -1,14 +1,13 @@
 package beans;
 
 public class CarFactory{
-	public static String carName;
+	public String carName;
 
-	public static String setCarName(String carName) {
-		CarFactory.carName=carName;
-		return carName; 
+	public void setCarName(String carName) {
+		this.carName=carName;		
 	}
 	
-	public static Car getInstance() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+	public Car getInstance() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		 Car c = (Car)Class.forName(carName).newInstance();
 		 System.out.println(c);
 		 return c;
--- Spring_Hello_world/src/beans/Client.java
+++ Spring_Hello_world/src/beans/Client.java
@@ -9,26 +9,9 @@ public class Client {
 		// TODO Auto-generated method stub
 		String files[] = {"resources/Spring.xml"};
 		ApplicationContext factory = new ClassPathXmlApplicationContext(files);
-		Car c = (Car)factory.getBean("cf");
+		Car c = (Car)factory.getBean("c");
 		c.drive();
-		/*Resource r = new ClassPathResource("resources/Spring.xml");
-		BeanFactory factory = new XmlBeanFactory(r);
-		Object o =factory.getBean("t");
-		Test test = (Test)o;
-		test.hello("Abhishek");*/
-		/*try {
-		Class c = Class.forName("beans.Test");
-		Constructor cons[] = c.getDeclaredConstructors();
-		cons[0].setAccessible(true);
-		Object o = cons[0].newInstance(null);
-		Test t = (Test)o;
-		t.hello();
-		}
-		catch(Exception e) {
-			
-		}*/
-		/*System.out.println(o==o1);
-		System.out.println(o==o2);*/
+		
 	}
 
 }
